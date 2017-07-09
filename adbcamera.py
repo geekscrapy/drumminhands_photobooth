b70d0309f8ce2f2074067e50c4d1084cb9977b3a
Funvacuum <gitkraken.fk567kxa1m@pkt5.com>
2017-07-09 14:42:50 +0100

    Bug fix - , shell=True

--- adbcamera.py
+++ adbcamera.py
@@ -14,30 +14,30 @@ class camera(object):
 
 
 	def power_toggle(self):
-		ret = subprocess.call('adb shell "input keyevent KEYCODE_POWER"')
+		ret = subprocess.call('adb shell "input keyevent KEYCODE_POWER"', shell=True)
 		if ret == 0:
 			self.status = not self.status
 
 	def take(self):
-		return subprocess.call('adb shell "input keyevent KEYCODE_CAMERA"')
+		return subprocess.call('adb shell "input keyevent KEYCODE_CAMERA"', shell=True)
 
 	def get_pic(self, filename):
 
 		# Get the filename of the latest picture
-		cmd = 'cd /sdcard/DCIM/Camera/; IFS=$\'\n\'; output=(`ls -l`); lines=${#output[@]}; IFS=$\' \'; file_line=(${output[$((lines-1))]}); file_name=(); index=0; for part in ${file_line[@]}; do if [[ $index -gt 4 ]]; then file_name+=($part); fi; index=$((index+1)); done; echo ${file_name[1]}'
-		cap_name = subprocess.check_output(['adb', 'shell', cmd])
+		cmd = '"cd /sdcard/DCIM/Camera/; IFS=$\'\n\'; output=(`ls -l`); lines=${#output[@]}; IFS=$\' \'; file_line=(${output[$((lines-1))]}); file_name=(); index=0; for part in ${file_line[@]}; do if [[ $index -gt 4 ]]; then file_name+=($part); fi; index=$((index+1)); done; echo ${file_name[1]}"'
+		cap_name = subprocess.check_output(['adb', 'shell', cmd], shell=True)
 
-		if cap_name == '':
+		if cap_name.upper().find('JPG') != -1:
 			return False
 
 		# Then copy it from the camera
-		ret = subprocess.call('adb pull /sdcard/DCIM/Camera/'+cap_name+' '+config.file_path)
+		ret = subprocess.call('adb pull /sdcard/DCIM/Camera/'+cap_name+' '+config.file_path, shell=True)
 
 		if ret != 0:
 			return False
 
 		# Then rename it to what we planned!
-		ret = subprocess.call('mv '+config.file_path+cap_name+' '+config.file_path+filename)
+		ret = subprocess.call('mv '+config.file_path+cap_name+' '+config.file_path+filename, shell=True)
 		if ret != 0:
 			return False
 
---
+++ run-pb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Start adb then photobooth on boot
+sudo rm /tmp/5037
+sudo adb start-server
+sudo chmod 777 /tmp/5037
+
+cd drumminhands_photobooth
+sudo python drumminhands_photobooth.py
