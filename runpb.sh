b799ad8da4de675ad85cdc8fd5fd531d90be25f7
Funvacuum <gitkraken.fk567kxa1m@pkt5.com>
2017-07-11 19:04:45 +0100

    Moved blocking section to before we mod anything in run script

--- runpb.sh
+++ runpb.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
+echo 'Waiting for 60 seconds to breakout'
+sleep 60
+
 # Start adb then photobooth on boot
 sudo rm /tmp/5037
 sudo adb start-server
 sudo chmod 777 /tmp/5037
 
 cd drumminhands_photobooth
-
-
-echo 'Waiting for 60 seconds to breakout'
-sleep 60
 
 sudo python headless.py
